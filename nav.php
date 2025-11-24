e68484d6680161bcc78ee7196baf4d111e8d2335
Opilane TTHK <opilane@tthk.ee>
2025-11-24 12:31:39 +0200

    nav menüü muudetud

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="menu">
     <ul>
         <li>
             <a href="?leht=kodu.php">Kodu</a>
@@ -10,16 +10,15 @@
             <a href="?leht=jsVeebikalkulaator.php">JS veebikalk</a>
         </li>
         <li>
-            <a href="?leht=ajafunktsioonid.php">Ajafuktsioonid</a>
+            <a>Funktsioonid</a>
+            <ul class="dropdown">
+                <li><a href="?leht=tekstfunktsioonid.php">Tekstfuktsioonid</a></li>
+                <li><a href="?leht=matemaatilised.php">Matemaatilised fuktsioonid</a></li>
+                <li><a href="?leht=ajafunktsioonid.php">Ajafuktsioonid</a></li>
+            </ul>
         </li>
         <li>
-            <a href="?leht=tekstfunktsioonid.php">Tekstfuktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=matemaatilised.php">Matemaatilised fuktsioonid</a>
-        </li>
-        <li>
-            <a href="https://merkulova.thkit.ee" target="_blank">Vana index.html</a>
+            <a href="savvasmirnyagin24.thkit.ee" target="_blank">Vana index.html</a>
         </li>
         <li>
             <a href="?leht=gitKasud.php">GIT käsud</a>
